d46098150ab00b04313e5f298f6cbcdfc4057ab8
RMSuyama <rafaelmoreirasuyama@gmail.com>
2023-02-25 01:23:01 -0300

    Mensagem de commit

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+    
+    <template>
+  <div>
+    <h2>{{ title }}</h2>
+    <ul>
+      <li v-for="task in tasks" :key="task.id">{{ task.name }}</li>
+    </ul>
+  </div>
+</template>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
